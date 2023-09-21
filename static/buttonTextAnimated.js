da121f6db123de93547071395c1e8f5b57259a2c
bukulele <zonough@gmail.com>
2023-09-21 18:52:27 +0400

    animation of button texts finished; icon changed

--- static/buttonTextAnimated.js
+++ static/buttonTextAnimated.js
@@ -1,26 +1,48 @@
-let buttonTextAnimated = document.querySelector('.button-text-animation__container');
-let textsContainer = buttonTextAnimated.querySelector('div');
-let buttonTexts = buttonTextAnimated.querySelectorAll('p');
-let navbarContainer = document.querySelector('.solidblockmenu');
+const buttonTextAnimated = document.querySelector('.button-text-animation__container');
+const textsContainer = buttonTextAnimated.querySelector('.button-texts__container');
 let maxTextWidth = 0;
+let textHeight;
+let currentButtonText = 0;
+let currentTextContainerTopPosition = 0;
+
+const buttonTexts = ['MyTinyDick', 'SPH', 'Femdom', 'MyTinyDick'];
 
 for (let text of buttonTexts) {
-  if (text.offsetWidth > maxTextWidth) {
-    maxTextWidth = text.offsetWidth;
+  let pElem = document.createElement('p');
+  pElem.innerHTML = text;
+  textsContainer.insertAdjacentElement('beforeend', pElem);
+
+  if (pElem.offsetWidth > maxTextWidth) {
+    maxTextWidth = pElem.offsetWidth;
   }
 }
+textHeight = textsContainer.firstElementChild.offsetHeight;
+
 buttonTextAnimated.style.width = `${maxTextWidth}px`;
+buttonTextAnimated.style.height = `${textHeight}px`;
 
-function moveButtonTextsUp (event) {
-  if (event.target.className.includes('content-animated')) {
-    textsContainer.style.top = `-17px`;
+moveButtonTextsUp();
+
+function moveButtonTextsUp () {
+  if (!textsContainer.className.includes('button-texts__container--animated')) {
+    textsContainer.classList.add('button-texts__container--animated');
   }
+
+  if (currentButtonText === buttonTexts.length - 1) {
+    currentButtonText = 0;
+    currentTextContainerTopPosition = currentButtonText * textHeight;
+    textsContainer.classList.remove('button-texts__container--animated');
+    textsContainer.style.top = `${-currentTextContainerTopPosition}px`;
+    setTimeout(() => {
+      currentButtonText += 1;
+      currentTextContainerTopPosition = currentButtonText * textHeight;
+      textsContainer.classList.add('button-texts__container--animated');
+      textsContainer.style.top = `${-currentTextContainerTopPosition}px`;
+    }, 0);
+  } else {
+    currentButtonText += 1;
+    currentTextContainerTopPosition = currentButtonText * textHeight;
+    textsContainer.style.top = `${-currentTextContainerTopPosition}px`;
+  }
+  setTimeout(moveButtonTextsUp, 3000);
 }
-
-function moveButtonTextsDown (event) {
-  if (event.target.className.includes('content-animated')) {
-    textsContainer.style.top = `0px`;
-  }}
-
-navbarContainer.addEventListener('mouseover', moveButtonTextsUp);
-navbarContainer.addEventListener('mouseout', moveButtonTextsDown);
